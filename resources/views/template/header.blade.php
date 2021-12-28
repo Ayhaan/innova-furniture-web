7f682b24d7906c10deb3a488525f7f9adbef697e
Ayhan <caliskanayhan@hotmail.fr>
2021-12-28 23:22:43 +0100

    icon flag language add

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -30,14 +30,36 @@
                                 <li><a href="{{ route('faq') }}">F.A.Q</a></li>
                                 <li><a href="{{ route('moving') }}">Moving</a></li>
                                 <li><a href="{{ route('contact') }}">Contact</a></li>
-                                <li class="ml-2"><a href="###">{{ Config::get('languages')[App::getLocale()] }}</a>
+                                <li class="ml-2">
+                                    <a href="#">
+                                        <img class="mr-1" height="18px" src="{{ asset('img/flag/'. Config::get('languages')[App::getLocale()]['flag-icon'] . ".png") }}" alt="lang-{{ Config::get('languages')[App::getLocale()]['flag-icon'] }}">
+                                        {{ Config::get('languages')[App::getLocale()]['display'] }}
+                                    </a>
                                     <ul class="submenu">
                                         @foreach (Config::get('languages') as $lang => $language)
                                             @if ($lang != App::getLocale())
-                                                    <li><a href="{{ route('lang.switch', $lang) }}">{{$language}}</a></li>
+                                                <li>
+                                                    <a href="{{ route('lang.switch', $lang) }}" class="d-flex align-items-center">
+                                                        <img class="mr-1" height="18px" src="{{ asset('img/flag/'. $language['flag-icon'] . ".png") }}" alt="lang-{{ $language['flag-icon'] }}">
+                                                        {{$language['display']}}
+                                                    </a>
+                                                </li>
                                             @endif
                                         @endforeach
+                                       
                                     </ul>
+                                    {{-- <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        @foreach (Config::get('languages') as $lang => $language)
+                                            @if ($lang != App::getLocale())
+                                                    <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a>
+                                            @endif
+                                        @endforeach
+                                        </div>
+                                    </li> --}}
                                 </li>
                             </ul>
                         </nav>
