467b4b37347bfcde4d19f0e2e81a3308c1e2f377
Ayhan <caliskanayhan@hotmail.fr>
2022-01-05 13:42:57 +0100

    update design

--- resources/views/template/contact/maps.blade.php
+++ resources/views/template/contact/maps.blade.php
@@ -1,4 +1,4 @@
 <div  class="d-none d-sm-block pb-4">
-    <iframe width="100%" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=807&amp;height=480&amp;hl=en&amp;q=KREKELENDRIES%2011%20Merchtem+(Innova%20Furniture)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='https://www.symptoma.fr/fr/info/covid-19#info'>COVID-19</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=678ad3e9f943654fe481c810342f6039e0e3d0a4'></script>
+    <iframe width="100%" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=600&amp;height=480&amp;hl=en&amp;q=Krekelendries%2011%20Merchtem+(Innova%20Furniture)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href="https://maps-generator.com/fr">Maps-Generator</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=d27dd24de5680f5832d0215b3836bb120f76b4f7'></script>
 </div>
 <hr id="movingContact" style="margin: 50px 0">
